cbfddac3070e38244b1ca7b4cd9478fa450a9e34
KAGA2023 <jinwoo230@naver.com>
2026-02-23 23:57:07 +0900

    국가 데이터테이블 수정 완료 외교 메뉴 리모델링, 타일 호버 메뉴 리모델링, 승리/패배 메뉴 윤곽만 모델링
    유닛 정보 메뉴, 소식 메뉴 제작 및 모델링 해야함

---
+++ CivilizationV260223/Source/Civilization/Widget/DiplomacyWidget/DiplomacyUI.h
@@ -0,0 +1,109 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DiplomacyUI.generated.h"
+
+class ASuperPlayerState;
+
+// 외교 UI 닫기 버튼 클릭 델리게이트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDiplomacyUICloseButtonClicked);
+
+UCLASS()
+class CIVILIZATION_API UDiplomacyUI : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	// 닫기 버튼 클릭 델리게이트
+	UPROPERTY(BlueprintAssignable, Category = "Diplomacy Events")
+	FOnDiplomacyUICloseButtonClicked OnDiplomacyUICloseButtonClicked;
+	
+	// 플레이어 정보 설정 함수
+	UFUNCTION(BlueprintCallable, Category = "Diplomacy UI")
+	void SetupForPlayer(int32 TargetPlayerIndex, ASuperPlayerState* TargetPlayerState);
+
+	// 대상 플레이어 인덱스 가져오기
+	UFUNCTION(BlueprintCallable, Category = "Diplomacy UI")
+	int32 GetTargetPlayerIndex() const { return CurrentTargetPlayerIndex; }
+
+	// 버튼 상태 업데이트 함수
+	UFUNCTION(BlueprintCallable, Category = "Diplomacy UI")
+	void UpdateButtonStates();
+
+private:
+	// DiplomacyManager 가져오기 헬퍼 함수
+	class UDiplomacyManager* GetDiplomacyManager() const;
+
+	// 현재 라운드 가져오기 헬퍼 함수
+	int32 GetCurrentRound() const;
+
+protected:
+	virtual void NativeConstruct() override;
+
+	// 버튼 클릭 핸들러
+	UFUNCTION()
+	void OnSendGiftBtnClicked();
+
+	UFUNCTION()
+	void OnOfferAllianceBtnClicked();
+
+	UFUNCTION()
+	void OnOfferPeaceBtnClicked();
+
+	UFUNCTION()
+	void OnDenounceBtnClicked();
+
+	UFUNCTION()
+	void OnDeclareWarBtnClicked();
+
+	UFUNCTION()
+	void OnCloseBtnClicked();
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UTextBlock* CountryTxt = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UImage* CountryImg = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UImage* KingImg = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UTextBlock* ConversationTxt = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UImage* LoseImg = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UTextBlock* StateWithMeTxt = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UTextBlock* AttitudeWithMeTxt = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UButton* SendGiftBtn = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UButton* OfferAllianceBtn = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UButton* OfferPeaceBtn = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UButton* DenounceBtn = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UButton* DeclareWarBtn = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UButton* CloseBtn = nullptr;
+
+private:
+	// 대상 플레이어 인덱스
+	UPROPERTY()
+	int32 CurrentTargetPlayerIndex = -1;
+};
+
